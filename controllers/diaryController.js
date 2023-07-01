61277cfc6aa56515b88745aec02134f8665ff336
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-07-01 09:56:41 -0400

    fixed delete button Now deletes meals from planner

--- controllers/diaryController.js
+++ controllers/diaryController.js
@@ -37,27 +37,28 @@ exports.postMacros = (req, res) => {
 };
 
 exports.deleteMacro = (req, res) => {
+  const recipeId = req.params.id;
+
+  console.log(recipeId);
+
   knex("diary")
     .delete()
-    .where({ recipe_id: req.body.recipe_id })
+    .where({ recipe_id: recipeId })
     .then((data) => {
-      if (!data) {
-        res.status(404).send(`No recipe with ID ${req.body.recipe_id} exists`);
+      if (data === 0) {
+        res
+          .status(404)
+          .send(`No recipe with ID ${req.params.recipe_id} exists`);
       } else {
-        knex("diary")
-          .delete()
-          .where({ recipe_id: req.body.recipe_id })
-          .then(() =>
-            res
-              .status(204)
-              .send(`Recipe with id: ${req.body.recipe_id} has been deleted`)
-          );
+        res
+          .status(204)
+          .send(`Recipe with id: ${req.params.recipe_id} has been deleted`);
       }
     })
     .catch((err) => {
-      console.log(req.body);
+      console.log(req.params.recipe_id);
       res
-        .status(404)
-        .send(`Error deleting recipe ${req.body.recipe_id}: ${err}`);
+        .status(500)
+        .send(`Error deleting recipe ${req.params.recipe_id}: ${err}`);
     });
 };
