020c50f47f0b5df9d47e863ae6316bb1901862bd
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-10-14 10:11:29 -0400

    imported bcrypt.js package into file

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -1,5 +1,5 @@
 const knex = require("knex")(require("../knexfile"));
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const db = require("../db");
 const uniqid = require("uniqid");
 
