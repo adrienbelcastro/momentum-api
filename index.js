fc9b7d3bb90535615e8f9a3b1d491aa9b3f538e9
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-11-11 20:01:30 -0500

    removed headers

--- index.js
+++ index.js
@@ -17,16 +17,6 @@ const registrationRoutes = require("./routes/registrationRoutes");
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  if (
-    req.method === "POST" &&
-    req.headers["content-type"] !== "application/json"
-  ) {
-    return res.status(400).send("This is not a VALID JSON!");
-  }
-  next();
-});
-
 app.use("/meals", diaryRoute);
 app.use("/article", articlesRoutes);
 app.use("/login", loginRoutes);
