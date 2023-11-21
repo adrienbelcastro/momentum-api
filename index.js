77e45a1ce98bd3084802121e39dea1e1aa2a04b3
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-11-21 11:21:53 -0500

    switched cors url to main branch api url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect();
 const corsOptions = {
-  origin: "https://momentum-git-feature-api-test-adrienbelcastro.vercel.app",
+  origin: "https://momentum-iota.vercel.app",
   credentials: true,
   optionSuccessStatus: 200,
 };
